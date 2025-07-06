5c5a1ba1fa0bb0d8ef15c740884d27d4188d7216
Frimpong Bright Adjei <owurakufrimpong772@gmail.com>
2025-07-06 23:44:37 +0000

    Update App.js
    updated the login logic function by adding a local storage to the app to save and compare the logins from the sign up page before granting access to the user

--- App.js
+++ App.js
@@ -6,9 +6,11 @@ import SignUP from './SignUP';
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [signup, setSignup] = useState(false);
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [error, setError] = useState('');
+  
  /*  const[checked,setChecked]=useState(false); */
   const handleLogin = (e) => {
     e.preventDefault();
@@ -28,17 +30,45 @@ function App() {
       setError('Invalid email format');
       return;
     }
+    if(email!==JSON.parse(localStorage.getItem('userinfo'))[0] || password!==JSON.parse(localStorage.getItem('userinfo'))[1]){
+      setError('Enter correct email and password');
+      return;
+    }
     
     setIsLoggedIn(true);
     setError('');
   };
+  const handlesignup = (e) => {
+    e.preventDefault();
+    
+    //Emailf and password validation logic
+    if (!email || !password) {
+      setError('Email and password are required');
+      return;
+    }
+    if (password.length < 6) {
+      setError('Password must be at least 6 characters long');
+      return;
+    }
+    // Simple email format validation
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(email)) {
+      setError('Invalid email format');
+      return;
+    }
+    setSignup(true);
+    setError('');
+     
+    localStorage.setItem('userinfo',JSON.stringify([email,password]))
+
+  }
   
   return (
     <div className="App">
       <Routes>
 
       <Route path="/" element={<Loginpage handleLogin={handleLogin} email={email} setEmail={setEmail} password={password} setPassword={setPassword} error={error} setError={setError}/>}/>
-      <Route path="/signup" element={<SignUP/>}/>
+      <Route path="/signup" element={<SignUP handlesignup={handlesignup} email={email}setEmail={setEmail} password={password} setPassword={setPassword} error={error} setError={setError}/>}/>
       </Routes>
     </div>
   );
